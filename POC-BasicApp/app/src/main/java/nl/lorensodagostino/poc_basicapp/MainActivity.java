f2aa3d059357e7f9c38573baa2b4674031229865
Lorenso D'Agostino <lorenso.dagostino@gmail.com>
2017-09-01 15:24:31 +0200

    Begonnen aan Start Another Activity Demo

--- POC-BasicApp/app/src/main/java/nl/lorensodagostino/poc_basicapp/MainActivity.java
+++ POC-BasicApp/app/src/main/java/nl/lorensodagostino/poc_basicapp/MainActivity.java
@@ -1,7 +1,12 @@
 package nl.lorensodagostino.poc_basicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,6 +18,10 @@ public class MainActivity extends AppCompatActivity {
 
     /** Called when the user taps the Send button */
     public void sendMessage(View view) {
-        // Do something in response to button
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
     }
 }
