799d67cb036a027fc2df0a66ff5e81ec1e876cdb
LorensoD <lorenso.dagostino@gmail.com>
2017-09-27 14:14:07 +0200

    Added Camera Permission

--- CSI_week_1/app/src/main/java/csi/fhict/org/csi_week_1/ReportActivity.java
+++ CSI_week_1/app/src/main/java/csi/fhict/org/csi_week_1/ReportActivity.java
@@ -1,9 +1,14 @@
 package csi.fhict.org.csi_week_1;
 
 import android.Manifest;
+import android.content.ContentResolver;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.hardware.Camera;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
@@ -11,10 +16,15 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import java.io.File;
 
 public class ReportActivity extends AppCompatActivity {
 
     private static final byte CALL_PERMISSION = 0;
+    private static final byte CAMERA_PERMISSION = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,6 +38,16 @@ public class ReportActivity extends AppCompatActivity {
                 checkCallPermission();
             }
         });
+
+        Button btnMakePic = (Button)findViewById(R.id.btnMakePic);
+        btnMakePic.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                checkCameraPermission();
+            }
+        });
+
+
     }
 
     //Controleer of de benodigde permissions gegeven zijn en vraagt er om als dat niet het geval is
@@ -47,7 +67,7 @@ public class ReportActivity extends AppCompatActivity {
     @Override
     public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
         switch (requestCode) {
-            case CALL_PERMISSION: {
+            case CALL_PERMISSION:
                 // If request is cancelled, the result arrays are empty.
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                     callDaPolice();
@@ -58,14 +78,89 @@ public class ReportActivity extends AppCompatActivity {
                     // functionality that depends on this permission.
                 }
                 break;
-            }
+//
+//            case CAMERA_PERMISSION:
+//                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    dispatchTakePictureIntent();
+//
+//                } else {
+//
+//                    // permission denied, boo! Disable the
+//                    // functionality that depends on this permission.
+//                }
+//                break;
 
-            // other 'case' lines to check for other
-            // permissions this app might request
         }
+
+
     }
 
     public void callDaPolice(){
         startActivity(new Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "112", null)));
     }
+
+    protected void checkCameraPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+            if(ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
+                //Laat een verklaring aan de gebruiker zien
+            } else {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION);
+            }
+        } else {
+            dispatchTakePictureIntent();
+        }
+    }
+
+    public void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File photo = new File(Environment.getExternalStorageDirectory(),  "Pic.jpg");
+
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photo));
+        //imageUri = Uri.fromFile(photo);
+        startActivityForResult(intent, CAMERA_PERMISSION);
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, CAMERA_PERMISSION);
+        }
+    }
+
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//        switch (requestCode) {
+//            case CAMERA_PERMISSION:
+//                if (resultCode == this.RESULT_OK) {
+//                    Uri selectedImage = imageUri;
+//                    getContentResolver().notifyChange(selectedImage, null);
+//                    ImageView imageView = (ImageView) findViewById(R.id.plaatjeBoef);
+//                    ContentResolver cr = getContentResolver();
+//                    Bitmap bitmap;
+//                    try {
+//                        bitmap = android.provider.MediaStore.Images.Media
+//                                .getBitmap(cr, selectedImage);
+//
+//                        imageView.setImageBitmap(bitmap);
+//                        Toast.makeText(this, selectedImage.toString(),
+//                                Toast.LENGTH_LONG).show();
+//                    } catch (Exception e) {
+//                        Toast.makeText(this, "Failed to load", Toast.LENGTH_SHORT)
+//                                .show();
+//                        Log.e("Camera", e.toString());
+//                    }
+//                }
+
+
+//    public void openCamera() {
+//        camera c = null;
+//        try {
+//            c = Camera.open(); // attempt to get a Camera instance
+//        } catch (Exception e) {
+//            // Camera is not available (in use or does not exist)
+//        }
+//        return c; // returns null if camera is unavailable
+//    }
 }
